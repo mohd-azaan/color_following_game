52fc8890832f4a2ac1102cc6f181d3a7bfd2c705
Mohammed Azaan <azaanpeshmam45@gmail.com>
2023-12-25 17:20:33 +0530

    commit

--- color_following_game/main.py
+++ color_following_game/main.py
@@ -3,78 +3,79 @@ import random
 import csv
 
 def open_game():
+    player_name = name_entry.get()
     home.destroy()  # Close the home window
-    start_game()
+    start_game(player_name)
 
-def start_game():
+def start_game(player_name):
     root = CTk()
     root.geometry("500x500")
     root.title("Color-Following-Game")
 
-    # Function to print a message when a button is clicked
+    
+
     def button_clicked(direction):
         print(f"Button {direction} clicked.")
         check_sequence(direction)
 
-    # Check if the clicked button is in the correct sequence
     def check_sequence(direction):
-        nonlocal sequence_index, score, highest_score
+        nonlocal sequence_index, score, highest_score, player_name
         if direction == sequence[sequence_index]:
             sequence_index += 1
             score += 1
             if score > highest_score:
                 highest_score = score
+                highest_player = player_name
         else:
             print("Incorrect sequence! Game over.")
-            save_highest_score()
+            save_highest_score(highest_score, highest_player)
             root.destroy()
-            rematch_prompt()
+            rematch_prompt(player_name)
 
         if sequence_index == len(sequence):
             update_sequence()
 
-    # Update the sequence and show it
     def update_sequence():
         nonlocal sequence, sequence_index
         sequence.append(random.choice(["Top", "Left", "Bottom", "Right"]))
         sequence_index = 0
         print("New sequence:", sequence)
         show_sequence()
+        guess_label.configure(text="Now you can start guessing!")
 
-    # Show the current sequence by highlighting the buttons
     def show_sequence():
         nonlocal sequence
         for direction in sequence:
             if direction == "Top":
-                top_button.configure(fg_color="#FF0000")  # Change to red
+                top_button.configure(fg_color="#FFFFFF")  # Change to white
                 root.update()
                 root.after(1000)  # Show for 1 second
                 top_button.configure(fg_color="#65B741")  # Revert to green
                 root.update()
                 root.after(500)  # Delay between button flashes
             elif direction == "Left":
-                left_button.configure(fg_color="#FF0000")  # Change to red
+                left_button.configure(fg_color="#FFFFFF")  # Change to white
                 root.update()
                 root.after(1000)  # Show for 1 second
                 left_button.configure(fg_color="#BF3131")  # Revert to red
                 root.update()
                 root.after(500)  # Delay between button flashes
             elif direction == "Bottom":
-                bottom_button.configure(fg_color="#FF0000")  # Change to red
+                bottom_button.configure(fg_color="#FFFFFF")  # Change to white
                 root.update()
                 root.after(1000)  # Show for 1 second
                 bottom_button.configure(fg_color="#FFFB73")  # Revert to yellow
                 root.update()
                 root.after(500)  # Delay between button flashes
             elif direction == "Right":
-                right_button.configure(fg_color="#FF0000")  # Change to red
+                right_button.configure(fg_color="#FFFFFF")  # Change to white
                 root.update()
                 root.after(1000)  # Show for 1 second
                 right_button.configure(fg_color="#4CB9E7")  # Revert to blue
                 root.update()
                 root.after(500)  # Delay between button flashes
 
-    def rematch_prompt():
+    def rematch_prompt(player_name):
         rematch_window = CTk()
         rematch_window.geometry("300x100")
         rematch_window.title("Rematch")
@@ -83,10 +84,10 @@ def start_game():
 
         def play_again():
             rematch_window.destroy()
-            start_game()
+            start_game(player_name)
 
         def end_game():
-            save_highest_score()
+            save_highest_score(highest_score, highest_player)
             rematch_window.destroy()
 
         rematch_yes_button = CTkButton(rematch_window, text="Yes", width=8, height=1, command=play_again)
@@ -95,40 +96,41 @@ def start_game():
         rematch_no_button = CTkButton(rematch_window, text="No", width=8, height=1, command=end_game)
         rematch_no_button.pack(side="right")
 
-    def save_highest_score():
+    def save_highest_score(score, player_name):
         with open('highest.csv', 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([highest_score])
+            writer.writerow([score, player_name])
 
-    # Load the highest score from file
     try:
         with open('highest.csv', 'r') as file:
             reader = csv.reader(file)
-            highest_score = int(next(reader)[0])
+            highest_score, highest_player = map(str, next(reader))
+            highest_score = int(highest_score)
     except FileNotFoundError:
-        highest_score = 0
+        highest_score, highest_player = 0, "None"
+    highest_score_label = CTkLabel(root, text=f"Highest Score: {highest_score} by {highest_player}", fg_color="black")
+    highest_score_label.place(relx=0.8, rely=0.02, anchor="ne")
+    sequence = []
+    sequence_index = 0
+    score = 0
 
-    sequence = []  # Initialize the sequence
-    sequence_index = 0  # Initialize the sequence index
-    score = 0  # Initialize the score
-
-    # Top button - Green
-    top_button = CTkButton(root, text="", width=150, height=150, fg_color="#65B741", corner_radius=100, hover_color="#458230", command=lambda: button_clicked("Top"))
+    top_button = CTkButton(root, text="", width=150, height=150, fg_color="#65B741", corner_radius=100,
+                       hover_color="#458230", command=lambda: button_clicked("Top"))
     top_button.place(relx=0.5, rely=0.1, anchor="n")
 
-    # Left button - Red
-    left_button = CTkButton(root, text="", width=150, height=150, fg_color="#BF3131", corner_radius=100, hover_color="#8C1F1F", command=lambda: button_clicked("Left"))
+    left_button = CTkButton(root, text="", width=150, height=150, fg_color="#BF3131", corner_radius=100,
+                            hover_color="#8C1F1F", command=lambda: button_clicked("Left"))
     left_button.place(relx=0.1, rely=0.5, anchor="w")
 
-    # Bottom button - Yellow
-    bottom_button = CTkButton(root, text="", width=150, height=150, fg_color="#FFFB73", corner_radius=100, hover_color="#CCC64C", command=lambda: button_clicked("Bottom"))
+    bottom_button = CTkButton(root, text="", width=150, height=150, fg_color="#FFFB73", corner_radius=100,
+                            hover_color="#CCC64C", command=lambda: button_clicked("Bottom"))
     bottom_button.place(relx=0.5, rely=0.9, anchor="s")
 
-    # Right button - Blue
-    right_button = CTkButton(root, text="", width=150, height=150, fg_color="#4CB9E7", corner_radius=100, hover_color="#3384A5", command=lambda: button_clicked("Right"))
+    right_button = CTkButton(root, text="", width=150, height=150, fg_color="#4CB9E7", corner_radius=100,
+                            hover_color="#3384A5", command=lambda: button_clicked("Right"))
     right_button.place(relx=0.9, rely=0.5, anchor="e")
 
-    update_sequence()  # Start the first sequence
+    update_sequence()
     root.mainloop()
 
 home = CTk()
